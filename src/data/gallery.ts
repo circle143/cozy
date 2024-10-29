7aa71a291c349b9b71469a36b484eb97545ca1c3
rohan <rohanverma031@gmail.com>
2024-10-29 15:49:07 +0530

    feat: updated gallery images

---
+++ src/data/gallery.ts
@@ -0,0 +1,65 @@
+export const foodImages = [
+	"/gallery/food/IMG_6713.webp",
+	"/gallery/food/IMG_6752.webp",
+	"/gallery/food/IMG_6819.webp",
+	"/gallery/food/IMG_6830.webp",
+	"/gallery/food/IMG_6841.webp",
+	"/gallery/food/IMG_6866.webp",
+	"/gallery/food/IMG_6926.webp",
+	"/gallery/food/IMG_6934.webp",
+	"/gallery/food/IMG_6951.webp",
+];
+
+export const eventsImages = [
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+];
+
+export const ambianceImages = [
+	"/gallery/ambiance/IMG_7032.webp",
+	"/gallery/ambiance/IMG_7033.webp",
+	"/gallery/ambiance/IMG_7031.webp",
+	"/gallery/ambiance/IMG_7029.webp",
+	"/gallery/ambiance/IMG_7030.webp",
+	"/gallery/ambiance/IMG_7027.webp",
+	"/gallery/ambiance/IMG_7028.webp",
+	"/gallery/ambiance/IMG_7015.webp",
+	"/gallery/ambiance/IMG_7026.webp",
+	"/gallery/ambiance/IMG_7013.webp",
+	"/gallery/ambiance/IMG_7014.webp",
+	"/gallery/ambiance/IMG_7011.webp",
+	"/gallery/ambiance/IMG_7012.webp",
+	"/gallery/ambiance/IMG_7008.webp",
+	"/gallery/ambiance/IMG_7009.webp",
+	"/gallery/ambiance/IMG_7006.webp",
+	"/gallery/ambiance/IMG_7007.webp",
+	"/gallery/ambiance/IMG_7005.webp",
+];
+
+export const kitcherImages = [
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+	"/gallery/gallery.webp",
+];
+
+export const images = [
+	...ambianceImages,
+	...kitcherImages,
+	...foodImages,
+	...eventsImages,
+];
