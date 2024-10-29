e2f485424c8b64cceaeb048c1f52ba712a36c925
rohan <rohanverma031@gmail.com>
2024-10-29 13:40:31 +0530

    feat:added food menu

---
+++ src/data/menu.ts
@@ -0,0 +1,70 @@
+export const dish = [
+	{
+		dish_name: "Crème Cappucino (Mushroom, Broccoli, Chicken)",
+		dish_img: "/menu/one.webp",
+		dish_price: "₹275/325",
+		dish_content:
+			"Melted Butter with Chef made Stock and Finishing with Butter.",
+	},
+	{
+		dish_name: "Sup Mang Tay Cua",
+		dish_img: "/menu/two.webp",
+		dish_price: "₹375",
+		dish_content:
+			"Delicious Thai Tom Yum Soup is a spicy and sour soup made with mix vegetables.",
+	},
+	{
+		dish_name: "Tom Yum Kung",
+		dish_img: "/menu/three.webp",
+		dish_price: "₹375",
+		dish_content:
+			"Crispy romaine lettuce, crunchy croutons, parmesan get tossed with a creamy Caesar Salad dressing.",
+	},
+	{
+		dish_name: "Mexican Caesar - Veg/Chicken",
+		dish_img: "/menu/four.webp",
+		dish_price: "₹325/345",
+		dish_content:
+			"Salad has everything you are looking for in a summer salad – sweetness from the fruit, creaminess from the Cheese.",
+	},
+	{
+		dish_name: "Miso Tuna Salad",
+		dish_img: "/menu/five.webp",
+		dish_price: "₹445",
+		dish_content:
+			"Stuffed with vegetables & Chicken flavored with soya sauce and fried to make an appetising starter.",
+	},
+	{
+		dish_name: "Potato Factory - Plain/Peri Peri / Fully Loaded / Truffle",
+		dish_img: "/menu/six.webp",
+		dish_price: "₹245/275/325/375",
+		dish_content:
+			"Nagaland in the North East usually means spicy, it has enough complex heat from a blend of Homemade Sauce & The Ghost Chilli.",
+	},
+	{
+		dish_name: "Burrata Caprese Salad",
+		dish_img: "/menu/seven.webp",
+		dish_price: "₹395",
+		dish_content: "BAR BITES",
+	},
+	{
+		dish_name: "Stuffed Chicken Wings",
+		dish_img: "/menu/eight.webp",
+		dish_price: "₹450",
+		dish_content:
+			"Chicken wings stuffed with minced meat and grilled to perfection.",
+	},
+	{
+		dish_name: "Grilled Prawns",
+		dish_img: "/menu/nine.webp",
+		dish_price: "₹695",
+		dish_content: "Prawns marinated with spices and grilled till tender.",
+	},
+	{
+		dish_name: "Garlic Bread with Cheese",
+		dish_img: "/menu/ten.webp",
+		dish_price: "₹195",
+		dish_content:
+			"Garlic bread topped with mozzarella cheese and baked until golden.",
+	},
+];
