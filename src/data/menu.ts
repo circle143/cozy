ae81b63b749ca8e8ea21463e9d35efdb2b4afa0a
srdeveloper-ssci <srdeveloper@srisaiconsultancyinfosoft.in>
2026-02-24 12:40:21 +0530

    cozy website updated

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,70 +1,129 @@
 export const dish = [
 	{
-		dish_name: "Crème Cappucino (Mushroom, Broccoli, Chicken)",
-		dish_img: "/menu/one.webp",
-		dish_price: "₹275/325",
+	  dish_name: "Tandoori Veg Platter",
+	  dish_img: "/menu/one.png",
+	  dish_price: "₹1095",
+	  dish_content:
+		"An assortment of hara bhara kabab, paneer tikka, soya chaap, and dahi ke kabab.",
+	},
+	{
+	  dish_name: "Tandoori Non Veg Platter",
+	  dish_img: "/menu/two.jpg",
+	  dish_price: "₹1195",
+	  dish_content:
+		"An assortment of fish tikka, chicken seekh, chicken tikka, and mutton seekh.",
+	},
+	{
+	  dish_name: "Basil Paneer Tikka",
+	  dish_img: "/menu/three.jpg",
+	  dish_price: "₹450",
+	  dish_content:
+		"Paneer, bell peppers, and onions marinated in fresh basil pesto and grilled. An Italian twist on our favorite paneer tikka.",
+	},
+	{
+	  dish_name: "Pahadi Chicken Tikka",
+	  dish_img: "/menu/four.png",
+	  dish_price: "₹445/545",
+	  dish_content:
+		"Boneless chicken cubes marinated in green chutney and baked to perfection.",
+	},
+	{
+	  dish_name: "Mushroom Chilli",
+	  dish_img: "/menu/five.png",
+	  dish_price: "₹535",
+	  dish_content:
+		"An Oriental stir-fry specialty. Ginger mushroom chilli features button mushrooms delicately flavored with chilli.",
+	},
+	{
+	  dish_name: "Firangi Chicken Tikka",
+	  dish_img: "/menu/seven.png",
+	  dish_price: "₹535",
+	  dish_content:
+		"Chicken tikka kebabs in a richly spiced yogurt marinade. Grilled, barbecued, or griddled to perfection.",
+	},
+	{
+	  dish_name: "Potato Factory - Plain/Peri Peri/Fully Loaded",
+	  dish_img: "/menu/six.webp",
+	  dish_price: "₹295/330/390",
+	  dish_content: "A combo of mixed-flavored fries.",
+	},
+	{
+	  dish_name: "Fish Tikka",
+	  dish_img: "/menu/eight.png",
+	  dish_price: "₹655",
+	  dish_content:
+		"Cubes of boneless fish marinated with yogurt and Indian spices, grilled in a hot tandoor.",
+	},
+	{
+	  dish_name: "Crispy Corn with Chilli Pepper",
+	  dish_img: "/menu/nine.jpg",
+	  dish_price: "₹450",
+	  dish_content:
+		"Indo-Chinese style corn kernels deep-fried and sautéed in a delicious flavored sauce.",
+	},
+	{
+	  dish_name: "Bhatti Ka Murgh Half/Full",
+	  dish_img: "/menu/ten.png",
+	  dish_price: "₹475/835",
+	  dish_content:
+		"Chicken prepared with an authentic Indian recipe, marinated overnight in yogurt with a perfect blend of spices.",
+	},
+  ];
+
+export const drinks = [
+	{
+		dish_name: "Silk And Pink",
+		dish_img: "/bar/Silk&Pink.png",
+		dish_price: "₹749",
 		dish_content:
-			"Melted Butter with Chef made Stock and Finishing with Butter.",
+			"Vodka, Litchi Juice, Cranberry Juice, Green Cardmom, Triple Sec, Lime Juice",
 	},
 	{
-		dish_name: "Sup Mang Tay Cua",
-		dish_img: "/menu/two.webp",
-		dish_price: "₹375",
+		dish_name: "Sanorita",
+		dish_img: "/bar/Sanorita.jpg",
+		dish_price: "₹399",
 		dish_content:
-			"Delicious Thai Tom Yum Soup is a spicy and sour soup made with mix vegetables.",
+			"Orange Juice, Strawberry Crush, Lime Juice",
 	},
 	{
-		dish_name: "Tom Yum Kung",
-		dish_img: "/menu/three.webp",
-		dish_price: "₹375",
+		dish_name: "Beerita",
+		dish_img: "/bar/Beerita.png",
+		dish_price: "₹799",
 		dish_content:
-			"Crispy romaine lettuce, crunchy croutons, parmesan get tossed with a creamy Caesar Salad dressing.",
+			"Vodka, Kala Khata, Salt, Chaat Masala, Lime Juice, Top Up Beer",
 	},
 	{
-		dish_name: "Mexican Caesar - Veg/Chicken",
-		dish_img: "/menu/four.webp",
-		dish_price: "₹325/345",
+		dish_name: "Cucumber Lemonade Fizz",
+		dish_img: "/bar/CucumberLemonadeFizz.png",
+		dish_price: "₹399",
 		dish_content:
-			"Salad has everything you are looking for in a summer salad – sweetness from the fruit, creaminess from the Cheese.",
+			"Fresh Cucumber, Lime Juice, Black Papper,Top Up With Soda",
 	},
 	{
-		dish_name: "Miso Tuna Salad",
-		dish_img: "/menu/five.webp",
-		dish_price: "₹445",
+		dish_name: "Smoke Irish",
+		dish_img: "/bar/SmokeIrish.png",
+		dish_price: "₹799",
 		dish_content:
-			"Stuffed with vegetables & Chicken flavored with soya sauce and fried to make an appetising starter.",
+			"Ipish Whisky, Baileys, Chocolat Syrup & Espresso.",
 	},
 	{
-		dish_name: "Potato Factory - Plain/Peri Peri / Fully Loaded / Truffle",
-		dish_img: "/menu/six.webp",
-		dish_price: "₹245/275/325/375",
+		dish_name: "Chilli Guava",
+		dish_img: "/bar/ChilliGuava.png",
+		dish_price: "₹399",
+		dish_content: "Guava Juice, Chaat Masala, Black Papper, Black Salt, Lime Juice",
+	},
+	{
+		dish_name: "Cosmopolitan",
+		dish_img: "/bar/Cosmopolitian.png",
+		dish_price: "₹525",
+		dish_content: "A crisp, citrus-kissed classic with smooth vodka and a vibrant berry twist.",
+	},
+	{
+		dish_name: "Spicy Mango",
+		dish_img: "/bar/SpicyMango.png",
+		dish_price: "₹399",
 		dish_content:
-			"Nagaland in the North East usually means spicy, it has enough complex heat from a blend of Homemade Sauce & The Ghost Chilli.",
+			"Mango Juice, Chaat Masala, Black Pappe, Black Salt, Lime Juice",
 	},
-	{
-		dish_name: "Burrata Caprese Salad",
-		dish_img: "/menu/seven.webp",
-		dish_price: "₹395",
-		dish_content: "BAR BITES",
-	},
-	{
-		dish_name: "Stuffed Chicken Wings",
-		dish_img: "/menu/eight.webp",
-		dish_price: "₹450",
-		dish_content:
-			"Chicken wings stuffed with minced meat and grilled to perfection.",
-	},
-	{
-		dish_name: "Grilled Prawns",
-		dish_img: "/menu/nine.webp",
-		dish_price: "₹695",
-		dish_content: "Prawns marinated with spices and grilled till tender.",
-	},
-	{
-		dish_name: "Garlic Bread with Cheese",
-		dish_img: "/menu/ten.webp",
-		dish_price: "₹195",
-		dish_content:
-			"Garlic bread topped with mozzarella cheese and baked until golden.",
-	},
+
 ];
