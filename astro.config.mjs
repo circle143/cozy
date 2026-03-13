50352334629fa3e0e5accfb06b1ef99e78708819
Ankit Soni <ankit@srisaiconsultancyinfosoft.in>
2026-03-13 16:09:59 +0530

    Revert Astro config to static build (remove Vercel adapter)

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,11 +12,8 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), react()],
-  output: "server",
-  adapter: vercel(),
 });
