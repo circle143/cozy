82b93dda02134a6ac496ad650bff8c4f176f9a31
Ankit Soni <ankit@srisaiconsultancyinfosoft.in>
2026-03-13 15:53:36 +0530

    Update .gitignore to exclude .vercel build output

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index.655c6a06_BNCGjGvY.mjs
+++
@@ -1,6 +0,0 @@
-const hero = "_hero_oae6s_1";
-const styles = {
-	hero: hero
-};
-
-export { styles as s };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index.8c9cafd9_CC0KzNgi.mjs
+++
@@ -1,6 +0,0 @@
-const menu = "_menu_4hwey_1";
-const styles = {
-	menu: menu
-};
-
-export { styles as s };
